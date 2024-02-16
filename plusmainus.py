5e2c74dc2d493b0a2febfc35786bdd24ce796f70
Chibgatullah Minhaz <119798464+chibgatullahminhaz@users.noreply.github.com>
2024-02-16 19:47:52 +0600

    Add files via upload

---
+++ plusmainus.py
@@ -0,0 +1,35 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def plusMinus(arr):
+    positiveCounter = 0
+    negetiveCounter = 0
+    zeroCounter = 0
+
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            positiveCounter += 1
+        elif arr[i] < 0:
+            negetiveCounter += 1
+        else:
+            zeroCounter += 1
+
+    print("%f" % (positiveCounter / len(arr)))
+    print("%f" % (negetiveCounter / len(arr)))
+    print("%f" % (zeroCounter / len(arr)))
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
+
